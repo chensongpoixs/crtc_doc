090d33c806dc9830a1cb38a3c6adf362c90193a8
chensong <chensongbj25@gmail.com>
2022-05-05 21:41:42 +0800

    [stun协议格式.pptx]

---
+++ 1、STUN/casync_udp_socket.h
@@ -0,0 +1,30 @@
+﻿
+
+#ifndef _C_ASYNC_UDP_SOCKET_H_
+#define _C_ASYNC_UDP_SOCKET_H_
+
+
+namespace chen {
+	
+	
+	class casync_udp_socket
+	{
+	public:
+		casync_udp_socket();
+		~casync_udp_socket();
+		
+	public:
+		bool init(const char * ip, uint16_t port);
+		bool startup();
+		
+		
+		void stop();
+		void destroy();
+		
+	private:
+		
+		
+	};
+}
+
+#endif // _C_ASYNC_UDP_SOCKET_H_
